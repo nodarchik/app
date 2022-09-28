defd0e8431f31708b55f61b7fcf97b7fe355e3a1
nodarchik <chikadzee@gmail.com>
2022-09-28 22:56:16 +0400

    Second page for desktop is working with same looks as in figma

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -8,20 +8,22 @@
     <title>Laravel</title>
 </head>
 <body>
+<div class="heading__udt">User Data Table</div>
+<div class="body__head">
+    <div class="head__name">Name</div>
+    <div class="head__country">Country</div>
+    <div class="head__date">Date of Birth</div>
+</div>
+<div class="body__main">
 @unless(count($listings)==0)
 @foreach($listings as $listing)
-    <h2>
-        {{$listing['name']}}
-    </h2>
-    <p>
-        {{$listing['country']}}
-    </p>
-    <p>
-        {{$listing['date']}}
-    </p>
+  <div class="main__info">{{$listing['name']}}</div>
+  <div class="main__info">{{$listing['country']}}</div>
+  <div class="main__info">{{$listing['date']}}</div>
 @endforeach
 @else
     <p>No listings found</p>
 @endunless
+</div>
 </body>
 </html>
